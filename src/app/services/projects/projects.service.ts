4a280c9ebbe32348b6bc6ccba6e1dff3556d40ff
Grant <grant.whiting@socreate.it>
2019-12-21 11:44:43 -0800

    add title and content styles for the product pages

---
+++ src/app/services/projects/projects.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IProject, IProjectIdAndImage} from '../../types/project';
+import {customProjectsApi, projectsApi, weddingProjectsApi} from '../apis';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  constructor(private http: HttpClient) { }
+
+  getProjects(): Observable<IProject[]> {
+    return this.http.get<IProject[]>(projectsApi);
+  }
+
+  getProjectById(projectId: number): Observable<IProject> {
+    return this.http.get<IProject>(`${projectsApi}/${projectId}`);
+  }
+
+  getWeddingProjects(): Observable<IProjectIdAndImage[]> {
+    return this.http.get<IProjectIdAndImage[]>(weddingProjectsApi);
+  }
+
+  getCustomProjects(): Observable<IProjectIdAndImage[]> {
+    return this.http.get<IProjectIdAndImage[]>(customProjectsApi);
+  }
+
+}
