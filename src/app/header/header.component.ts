00ec3df6d4d9103fa6490c60e9d5c4d940537253
Grant <grant.whiting@socreate.it>
2020-06-13 16:59:46 -0700

    Add sticky nav on mobile nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,28 @@
-import {AfterViewInit, Component, EventEmitter, OnDestroy, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, HostBinding, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {NavigationComponent} from '../navigation/navigation.component';
 import {takeUntil} from 'rxjs/internal/operators/takeUntil';
 import {Subject} from 'rxjs/internal/Subject';
+import {StickyNavService} from '../services/sticky-nav/sticky-nav.service';
 
 @Component({
   selector: 'du-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements AfterViewInit, OnDestroy {
+export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(NavigationComponent, { static: false }) navComponent: NavigationComponent;
   @Output() toggleNavEmitter = new EventEmitter<void>();
+  stickyNav: boolean;
 
   private unsubscribe = new Subject<void>();
+
+  constructor(private stickyNavService: StickyNavService) {}
+
+  ngOnInit() {
+    this.stickyNavService.isNavSticky
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(sticky => this.stickyNav = sticky);
+  }
 
   ngAfterViewInit() {
     this.navComponent.toggleMobileNavEmitter
